ad13f00a016ddcd9ee9d2a28ca4ead563f1aacef
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-31 17:08:16 +0100

    create the reviews api view

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -38,7 +38,7 @@ def get_review(review_id):
     return jsonify(review.to_dict())
 
 
-@app_views.route("/reviews/<review_id>", methods=['POST'],
+@app_views.route("/reviews/<review_id>", methods=['DELETE'],
                  strict_slashes=False)
 def delete_review(review_id):
     """
